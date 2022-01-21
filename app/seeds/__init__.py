9a350eecba4f8d4f71d4e33f1261a4a36220ab94
andrew <andrewtran@hey.com>
2022-01-21 14:31:21 -0600

    seeds complete

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,10 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .accounts import seed_accounts, undo_accounts
+from .budgets import seed_budgets, undo_budgets
+from .categories import seed_categories, undo_categories
+from .transactions import seed_transactions, undo_transactions
+
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,6 +15,10 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_accounts()
+    seed_categories()
+    seed_transactions()
+    seed_budgets()
     # Add other seed functions here
 
 
@@ -17,4 +26,8 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_accounts()
+    undo_categories()
+    undo_transactions()
+    undo_budgets()
     # Add other undo functions here
