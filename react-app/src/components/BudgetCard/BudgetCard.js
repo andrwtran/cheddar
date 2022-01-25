08b1b99e902074742faf1b2f4a316945046eba94
andrew <andrewtran@hey.com>
2022-01-24 22:32:50 -0600

    more charts

--- react-app/src/components/BudgetCard/BudgetCard.js
+++ react-app/src/components/BudgetCard/BudgetCard.js
@@ -17,9 +17,9 @@ export default function BudgetCard() {
   const transactions = useSelector((state) => state.transaction.all);
 
   const today = new Date();
+  const days_in_month = new Date(today.getFullYear(), today.getMonth()+1, 0).getDate() -  today.getDate();
 
   const transactions_monthly = transactions.filter(transaction => new Date(transaction.trans_date).getMonth() === today.getMonth())
-
 
   useEffect(() => {
     dispatch(getBudgets());
@@ -30,11 +30,6 @@ export default function BudgetCard() {
 
   const bar_options = {
     indexAxis: 'y',
-    elements: {
-      bar: {
-        borderWidth: 2,
-      },
-    },
     responsive: true,
     plugins: {
       legend: {
@@ -42,37 +37,35 @@ export default function BudgetCard() {
       },
       title: {
         display: true,
-        text: 'Monthly Spending',
+        text: 'Category Spending',
+        color: 'black',
+        font: {
+          size: '20',
+          family: "'Montserrat', sans-serif",
+        },
       },
     },
   };
 
-const bar_labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const bar_labels = budgets.slice(1).map(budget => budget.budget_name);
+const bar_budget_data = budgets.slice(1).map(budget => budget.budget_amount);
+
+console.log(bar_labels);
+console.log(bar_budget_data);
 
 const bar_data = {
   labels: bar_labels,
   datasets: [{
     axis: 'y',
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
+    data: bar_budget_data,
     fill: false,
     backgroundColor: [
       'rgba(255, 99, 132, 0.2)',
       'rgba(255, 159, 64, 0.2)',
-      'rgba(255, 205, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(201, 203, 207, 0.2)'
     ],
     borderColor: [
       'rgb(255, 99, 132)',
       'rgb(255, 159, 64)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)',
-      'rgb(201, 203, 207)'
     ],
     borderWidth: 1
   }]
@@ -80,12 +73,13 @@ const bar_data = {
 
   let budget_monthly;
   let budget_left;
+  let budget_left_daily;
   let pie_data;
 
-  // const budget_total_rem = (budgets[0].budget_amount - spend_monthly)/(budgets[0].budget_amount)/100;
   if (budgets.length) {
     budget_monthly = budgets[0].budget_amount;
     budget_left = budget_monthly - spend_monthly;
+    budget_left_daily = budget_left / days_in_month;
     pie_data = {
       labels: ['Spent', 'Left'],
       datasets: [
@@ -106,20 +100,25 @@ const bar_data = {
   }
 
   const pie_options = {
-    // indexAxis: 'y',
-    // elements: {
-    //   bar: {
-    //     borderWidth: 2,
-    //   },
-    // },
     responsive: true,
     plugins: {
-      // legend: {
-      //   display: false,
-      // },
+      legend: {
+        labels: {
+          color: 'black',
+          font: {
+            size: '12',
+            family: "'Montserrat', sans-serif",
+          },
+        }
+      },
       title: {
         display: true,
         text: 'Monthly Budget',
+        color: 'black',
+        font: {
+          size: '20',
+          family: "'Montserrat', sans-serif",
+        },
       },
     },
   };
@@ -127,7 +126,6 @@ const bar_data = {
 
   return (
     <div className='BudgetCard'>
-      {/* <h3>Budget</h3> */}
       <span className='BudgetTotalChart'>
         {pie_data !== undefined && <Pie options={pie_options} data={pie_data} />}
       </span>
@@ -142,6 +140,7 @@ const bar_data = {
             <>
               <li>Monthly Spend ${spend_monthly.toFixed(2)}</li>
               <li>Budget Left ${(budgets[0].budget_amount - spend_monthly).toFixed(2)}</li>
+              <li>( ${budget_left_daily.toFixed(2)} per day )</li>
             </>
           }
         </ul>
