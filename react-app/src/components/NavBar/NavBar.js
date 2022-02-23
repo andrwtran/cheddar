dca3eca5b0227ce84d9e6ebd2a12a4ad73755feb
andrew <andrewtran@hey.com>
2022-02-23 17:39:29 -0600

    more styling

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -10,17 +10,18 @@ const NavBar = () => {
 
   if (sessionUser) return (
     <nav className='NavBar' style={{width:"99%"}}>
+      <span id='NavBarLogo'><i class="fas fa-cheese" style={{marginRight: 3, marginLeft: 2}}></i>CHEDDAR</span>
       <ul>
+        <li>
+          <span id='WelcomeMessage'>{`Welcome, ${sessionUser.username}!`}</span>
+        </li>
+        <li style={{marginLeft: 8}} className='NavBarDivider'>|</li>
         <li>
           <NavLink to='/'>
             <button>Home <i className="fas fa-home" /></button>
           </NavLink>
         </li>
-        {/* <li>|</li> */}
-        <li>
-          <span id='WelcomeMessage'>{`Welcome, ${sessionUser.username}!`}</span>
-        </li>
-        {/* <li>|</li> */}
+        <li className='NavBarDivider'>|</li>
         <li>
           <LogoutButton />
         </li>
@@ -29,20 +30,20 @@ const NavBar = () => {
     );
 
   return (
-    <nav className='NavBar'>
-      <ul>
+    <nav className='NavBar' style={{display: "block"}}>
+      <ul style={{justifyContent: "space-around", marginTop: 0}}>
         <li>
           <NavLink to='/'>
             <button>Home <i className="fas fa-home" /></button>
           </NavLink>
         </li>
-        {/* <li>|</li> */}
+        <li className='NavBarDivider'>|</li>
         <li>
           <NavLink to='/login'>
             <button>Login <i className="fas fa-sign-in-alt" /></button>
           </NavLink>
         </li>
-        {/* <li>|</li> */}
+        <li className='NavBarDivider'>|</li>
         <li>
           <NavLink to='/sign-up'>
             <button>Sign Up <i className="fas fa-user-plus" /></button>
